77356bfbbfa43108b92566ca4e2ceeac97f711b8
HUANG, Shaojun <jimmy_huang@live.com>
2018-02-17 14:37:38 +0800

    initial commit

--- cps_token_contract.sol
+++ cps_token_contract.sol
@@ -1,1 +1,279 @@
+pragma solidity ^0.4.18;
 
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+interface ERC20 {
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) payable public returns (bool);
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
+
+//Lock amount of fund and unlock automatically after deadline
+interface ERCFundLock{
+    function lockFund (uint cycle, uint numOfSeconds, uint256 amount) public;
+    function unlockFund (uint cycle) public;
+    event LockFund(address indexed from, uint deadline, uint256 amount);
+    event UnlockFund(address indexed from, uint deadline, uint256 amount);
+}
+
+//lock amount of fund
+//each time unlockFundGradient is called
+//unlock unlockAmount = (now - time locked)/(deadline - time locked)*amount
+//
+interface ERCFundLockUnlockGradient {
+    function lockFundUnlockGradient (uint cycle, uint numOfSeconds, uint256 amount) public;
+    function unlockFundGradient (uint cycle) public;
+
+    event UnlockFundGradient(address indexed from, uint cycle, uint unlockTimestamp, uint deadline, uint256 unlockAmount);
+}
+
+interface ERC223 {
+    function transfer(address to, uint value, bytes data) public;
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
+
+
+contract ERC223ReceivingContract {
+    function tokenFallback(address _from, uint _value, bytes _data) public;
+}
+
+contract CPSTestToken1 is ERC20,ERC223,ERCFundLock {
+
+    using SafeMath for uint;
+
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint256 internal _totalSupply;
+    uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
+    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
+    address public fundsWallet;           // Where should the raised ETH go?
+
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    mapping (uint => mapping (string => uint256)) internal fundLocks;
+
+    function lockFund (uint cycle, uint numOfSeconds, uint256 amount) public  {
+
+        require (cycle > 0 && cycle <= 5 && amount > 0 && numOfSeconds > 0 && (fundLocks[cycle]["deadline"] == 0||fundLocks[cycle]["deadline"] < now));
+        require (balances[fundsWallet] - totalLockAmount() > amount);
+        require (msg.sender == fundsWallet);
+
+        fundLocks[cycle]["deadline"] = now + numOfSeconds;
+        fundLocks[cycle]["amount"] = amount;
+
+        LockFund(msg.sender, fundLocks[cycle]["deadline"], amount);
+    }
+
+    function lockedDeadline(uint cycle) public view returns (uint256) {
+        return fundLocks[cycle]["deadline"];
+    }
+
+    function lockedAmount(uint cycle) public view returns (uint256) {
+        return fundLocks[cycle]["amount"];
+    }
+
+    function unlockFund (uint cycle) public{
+        require (cycle > 0 && cycle <= 5 &&
+        (fundLocks[cycle]["deadline"] < now) &&
+        fundLocks[cycle]["amount"] > 0
+        );
+        require (msg.sender == fundsWallet);
+
+        fundLocks[cycle]["deadline"] = 0;
+
+        var amount = fundLocks[cycle]["amount"];
+        fundLocks[cycle]["amount"] = 0;
+
+        LockFund(msg.sender, fundLocks[cycle]["deadline"], amount);
+    }
+
+    function totalLockAmount() public view returns (uint256) {
+        uint256 amount = 0;
+        for(uint i=1;i<=5;i++){
+            if(fundLocks[i]["deadline"] > now){
+                amount += fundLocks[i]["amount"];
+            }
+        }
+        return amount;
+    }
+
+    function CPSTestToken1(string name, string symbol, uint8 decimals, uint256 totalSupply) public {
+        _symbol = symbol;
+        _name = name;
+        _decimals = decimals;
+        _totalSupply = totalSupply;
+        balances[msg.sender] = totalSupply;
+        fundsWallet = msg.sender;
+        unitsOneEthCanBuy = 100;
+    }
+
+    function name()
+    public
+    view
+    returns (string) {
+        return _name;
+    }
+
+    function symbol()
+    public
+    view
+    returns (string) {
+        return _symbol;
+    }
+
+    function decimals()
+    public
+    view
+    returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply()
+    public
+    view
+    returns (uint256) {
+        return _totalSupply;
+    }
+
+
+    function() payable public {
+        totalEthInWei = totalEthInWei + msg.value;
+        uint256 amount = msg.value/10^decimals() * unitsOneEthCanBuy;
+        require (balances[fundsWallet] - totalLockAmount() >= amount);
+
+        balances[fundsWallet] = balances[fundsWallet] - amount;
+        balances[msg.sender] = balances[msg.sender] + amount;
+
+        Transfer(fundsWallet, msg.sender, amount); // Broadcast a message to the blockchain
+
+        //Transfer ether to fundsWallet
+        fundsWallet.transfer(msg.value);
+    }
+
+    function transfer(address _to, uint256 _value) payable public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+        if(msg.sender == fundsWallet){
+            require(_value <= balances[msg.sender] - totalLockAmount());
+        }
+
+        if(isContract(_to)) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            bytes memory _data = new bytes(1);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+        balances[_to] = SafeMath.add(balances[_to], _value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        if(_from == fundsWallet){
+            require(_value <= balances[_from] - totalLockAmount());
+        }
+
+        if(isContract(_to)) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            bytes memory _data = new bytes(1);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+
+        balances[_from] = SafeMath.sub(balances[_from], _value);
+        balances[_to] = SafeMath.add(balances[_to], _value);
+        allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function transfer(address _to, uint _value, bytes _data) public {
+        require(_value > 0 );
+        if(isContract(_to)) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value, _data);
+    }
+
+    function isContract(address _addr) private view returns (bool is_contract) {
+        uint length;
+        assembly {
+        //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+        }
+        return (length>0);
+    }
+
+}
